4c867011cca2b06593cb63bae184713949ff2079
Aditi Davange <101041462+Aditi5670@users.noreply.github.com>
2022-12-22 15:42:02 +0530

    Updates

---
+++ precache-manifest.299460ab546a0c8317302a8e144e7242.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e510a45acbf165cd9bb0",
+    "url": "/Resume/static/css/main.85ec6655.chunk.css"
+  },
+  {
+    "revision": "e510a45acbf165cd9bb0",
+    "url": "/Resume/static/js/main.e510a45a.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/Resume/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "6a706cc55faf29324bba",
+    "url": "/Resume/static/js/2.6a706cc5.chunk.js"
+  },
+  {
+    "revision": "830efd14847438b5fe92",
+    "url": "/Resume/static/js/runtime~main.830efd14.js"
+  },
+  {
+    "revision": "3eca34e341e0ca200ac0c0ba8251546a",
+    "url": "/Resume/static/media/Video1.3eca34e3.mp4"
+  },
+  {
+    "revision": "fdab19d773a54deb4a2cedb556fc2452",
+    "url": "/Resume/static/media/Video2.fdab19d7.mp4"
+  },
+  {
+    "revision": "fa9da4fa5f7fd3b548117aba15ebff01",
+    "url": "/Resume/static/media/Blastoserve.fa9da4fa.mp4"
+  },
+  {
+    "revision": "e6806e76b738bf939fb2dd1fd6a90862",
+    "url": "/Resume/static/media/Experience_letter.e6806e76.jpg"
+  },
+  {
+    "revision": "3438af98ca6bbe3386459edd79e1eb83",
+    "url": "/Resume/static/media/CET.3438af98.jpeg"
+  },
+  {
+    "revision": "5ac77abfe9ba530bd71d91c92fa44384",
+    "url": "/Resume/static/media/HSC.5ac77abf.png"
+  },
+  {
+    "revision": "94d0b0bbe9339ae49a0575ce6fcabed7",
+    "url": "/Resume/static/media/SSC.94d0b0bb.png"
+  },
+  {
+    "revision": "b6ebcec5f19362e2e3e339663153a052",
+    "url": "/Resume/index.html"
+  }
+];
